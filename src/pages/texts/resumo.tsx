f61c2e3d9f37bedc51135c37d12ac0dde47229cb
ezefferth <ezefferth96@gmail.com>
2022-04-12 17:26:14 -0400

    first commit

---
+++ src/pages/texts/resumo.tsx
@@ -0,0 +1,14 @@
+
+import styles from '../../styles/Home.module.scss';
+
+export default function Resumo() {
+
+  return (
+
+    <p className={styles.resumoText}>
+      Formado em engenharia de computação, técnico em tecnologia da informação pela
+      UFMT
+
+    </p>
+  )
+}
